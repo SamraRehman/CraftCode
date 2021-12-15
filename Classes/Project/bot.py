e689048711928d87fb2fa4fd029a5a77b944d4f2
OmarMiah <omiahcs@gmail.com>
2021-12-14 19:53:16 -0500

    Uploading finished project for crypto currency tracker

--- Classes/Project/bot.py
+++ Classes/Project/bot.py
@@ -1,6 +1,7 @@
 import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from tracker import get_prices
 
 telegram_bot_token = "5099437040:AAETg-rRGvoSY10qDkFLn17kxUNMMCmlpBM"
 
@@ -10,7 +11,17 @@ dispatcher = updater.dispatcher
 
 def start(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello World")
+    message = ""
+
+    crypto_data = get_prices()
+    for i in crypto_data:
+        coin = crypto_data[i]["coin"]
+        price = crypto_data[i]["price"]
+        change_day = crypto_data[i]["change_day"]
+        change_hour = crypto_data[i]["change_hour"]
+        message += f"Coin: {coin}\nPrice: ${price:,.2f}\nHour Change: {change_hour:.3f}%\nDay Change: {change_day:.3f}%\n\n"
+
+    context.bot.send_message(chat_id=chat_id, text=message)
 
 
 dispatcher.add_handler(CommandHandler("start", start))
